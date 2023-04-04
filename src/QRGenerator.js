70dcff92a813d35e90c3892056a91e070a84a7f7
Lee Salminen <leesalminen@gmail.com>
2023-04-04 17:05:53 -0600

    styling

--- src/QRGenerator.js
+++ src/QRGenerator.js
@@ -32,10 +32,10 @@ function QRGenerator() {
 
   return (
     <Container className="mt-5">
-      <h1 className="mb-5">💯 Free QR Code Generator</h1>
+      <h1 className="mb-1">💯 Free QR Code Generator</h1>
       <p>This little app just generates QR codes. It sounds simple, but so many other QR Generators insert ads, spam, and trickery these days. Here, you can generate QR codes with peace of mind. No tracking, no ads, no bullshit.</p>
       <p><small>PS- this app was created using AI (ChatGPT-4 LLM).</small></p>
-      <Form>
+      <Form className="mt-2">
         <InputGroup className="mb-3">
           <FormControl type="text" onChange={handleChange} value={text} placeholder="Enter text to generate QR code" />
         </InputGroup>
